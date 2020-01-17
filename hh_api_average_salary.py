05870023fef19e40f2d99c561dc99a40b03e5d0f
AraxnoAnarxo <araxnoanarxo@protonmail.com>
2020-01-18 01:33:34 +0300

    расчет средней зп по запросу Python

---
+++ hh_api_average_salary.py
@@ -0,0 +1,40 @@
+import requests
+import json
+import pprint
+
+# ДОБАВИТЬ РЕГИОН МОСКВА
+
+salary_list_from = [] # список с зарплатами от...
+salary_list_to = [] # список с зарплатами до...
+
+for n in range(20):
+    URL = 'https://api.hh.ru/vacancies'
+
+    params = {'text': 'Python',
+    'only_with_salary': True, # ДОБАВИТЬ РЕГИОН МОСКВА
+    'per_page': 100, 'page': n
+    }
+
+# {'id': '1', 'parent_id': '113', 'name': 'Москва', 'areas': []}
+
+    result = requests.get(URL, params = params).json()
+
+    for j in result['items']:
+        # print(j['salary']['from'], j['salary']['to'])
+        if j['salary']['from'] != None: # убираем зарплаты с пустным значением
+            salary_list_from.append(j['salary']['from']) # создаем лист с зарплатой от...
+        if j['salary']['to'] != None: # убираем зарплаты с пустным значением
+            salary_list_to.append(j['salary']['to']) # создаем лист с зарплатой до...
+
+print(salary_list_from, len(salary_list_from))
+print(salary_list_to, len(salary_list_to)
+
+# делим сумму списка зарплат от... на количество элементов списка
+salary_from_average = sum(salary_list_from)/len(salary_list_from)
+# делим сумму списка зарплат до... на количество элементов списка
+salary_to_average = sum(salary_list_to)/len(salary_list_to)
+
+print(f'Средняя зарплата: от {salary_from_average} до {salary_to_average}')
+
+# Всего было найдено вакансий:
+pprint.pprint(result['found'])
