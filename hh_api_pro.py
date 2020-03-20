6cfece7d4cdf9f73b2fb3afde3d336719299ff2e
AraxnoAnarxo <araxnoanarxo@protonmail.com>
2020-03-20 22:35:30 +0300

    актуальные требования к вакансии Python-разработчика (hh API)

--- hh_api_pro.py
+++ hh_api_pro.py
@@ -5,103 +5,81 @@ import pymorphy2
 import string
 
 
-URL = 'https://api.hh.ru/vacancies'
+def hh_requirements():
+    URL = 'https://api.hh.ru/vacancies'
 
 
 
-requirement_list = []
+    requirement_list = []
 
-for n in range(100):
-    params = {'text': 'Python-разработчик', 'page': n}
-    result = requests.get(URL, params=params).json()
-    result_items = result['items']
+    for n in range(100):
+        params = {'text': 'Python-разработчик', 'page': n}
+        result = requests.get(URL, params=params).json()
+        result_items = result['items']
+        results_all = result['found']
 
-    for i in result_items:
-        result_snippet = i['snippet']
-        requirement = result_snippet['requirement']
-        try:
-            # очистить текст c требованиями по вакансиям от знаков препинания и вставки "highlighttext". Убираем лишние пробелы
-            for i in string.punctuation:
-                requirement = requirement.replace(i, " ")
-                requirement = requirement.replace('highlighttext', "")
-                requirement = requirement.replace('   ', " ")
-                requirement = requirement.replace('  ', " ")
-            # requirement.replace(",", " ").replace("—", " ").replace(".", " ").replace(".", " ")\
-            #               .replace("«", " ").replace("-", " ").replace("»", " ")\
-            #               .replace("(", " ").replace(")", " ").replace("!", " ")\
-            #               .replace("?", " ").replace(";", " ").replace(":", " ").replace("-", " ")
-            requirement_list += requirement.split()
-        except AttributeError:
-            print(requirement)
+        for i in result_items:
+            if i['snippet'] != None:  # убираем snippet с пустным значением
+                result_snippet = i['snippet']
+                if result_snippet['requirement'] != None:  # убираем requirement с пустным значением
+                    requirement = result_snippet['requirement']
+
+                    # очистить текст c требованиями по вакансиям от знаков препинания и вставки "highlighttext". Убираем лишние пробелы
+                    for i in string.punctuation:
+                        requirement = requirement.replace(i, " ")
+                        requirement = requirement.replace('highlighttext', "")
+                        requirement = requirement.replace('   ', " ")
+                        requirement = requirement.replace('  ', " ")
+                requirement_list += requirement.split()
 
 
+    other_words = {"работа","опыт", "работы","на", "не", "под", "у", "в", "с", "перед", "до", "о", "по", "из-за", "от", "для",
+                    "из-под", "над", "без", "близ", "ввиду", "между", "возле", "рядом", "около",
+                    "отношении", "вокруг", "впереди", "в продожение", "вследствие", "течение",
+                    "из", "кроме", "от", "подле", "по мере", "после", "прежде", "против",
+                    "благодаря", "вопреки", "к", "согласно", "соответсвенно", "несмотря",
+                    "про", "сквозь", "через", "во", "за", "об", "обо", "в", "соответствии",
+                    "надо", "перед", "согласно", "связи", "при", "без", "до", "для", "из",
+                    "под", "пред", "при", "ввиду", "насчет", "помощи", "случае", "условии",
+                    "погодя", "спустя", "благодаря", "начиная", "несмотря", "считая", "после",
+                    "мимо", "внутри", "вдоль", "вдали", "вокруг", "и", "или", "без", "можно", "лет", "уверенный",
+                   "опыт", "знание", "работа", "python", "разработка", "понимание", "experience", "умение", "python.",
+                   "хороший", "of", "язык", "in", "программирование", "and", "высокий", "with", "коммерческий", "3", "владение",
+                   "отличный", "python,", "/", "принцип", "работать", "1", "год", "менее", "один", "2-х","использование","технический",
+                   "<highlighttext>python</highlighttext>", "года", "knowledge", "2", "иметь", "быть", "база", "написание",
+                   "писать", "дать", "навык", "уровень", "образование", "years", "<highlighttext>development</highlighttext>", "с...",
+                   "и...", "3-х","промышленный","3","базовый","паттерн","желание","данных","образование","разработки.","реляционный","как",
+                   "структура","основный","система","код","strong","or","создание","современный","3.","good","проектирование","работы...'",
+                   "необходимый","+", "лет.", "-", "х", "c", "5", "приложение", "разработчик", "rest", "development", "web"}
+
+    requirement_list_lower = list(map(lambda x: x.lower(), requirement_list))
+    words_lemmed = []
+    morph = pymorphy2.MorphAnalyzer()
+    for i in requirement_list_lower:
+        words_lemmed.append(morph.parse(i)[0].normal_form)
 
 
-other_words = {"работа","опыт", "работы","на", "не", "под", "у", "в", "с", "перед", "до", "о", "по", "из-за", "от", "для",
-                "из-под", "над", "без", "близ", "ввиду", "между", "возле", "рядом", "около",
-                "отношении", "вокруг", "впереди", "в продожение", "вследствие", "течение",
-                "из", "кроме", "от", "подле", "по мере", "после", "прежде", "против",
-                "благодаря", "вопреки", "к", "согласно", "соответсвенно", "несмотря",
-                "про", "сквозь", "через", "во", "за", "об", "обо", "в", "соответствии",
-                "надо", "перед", "согласно", "связи", "при", "без", "до", "для", "из",
-                "под", "пред", "при", "ввиду", "насчет", "помощи", "случае", "условии",
-                "погодя", "спустя", "благодаря", "начиная", "несмотря", "считая", "после",
-                "мимо", "внутри", "вдоль", "вдали", "вокруг", "и", "или", "без", "можно", "лет", "уверенный",
-               "опыт", "знание", "работа", "python", "разработка", "понимание", "experience", "умение", "python.",
-               "хороший", "of", "язык", "in", "программирование", "and", "высокий", "with", "коммерческий", "3", "владение",
-               "отличный", "python,", "/", "принцип", "работать", "1", "год", "менее", "один", "2-х","использование","технический",
-               "<highlighttext>python</highlighttext>", "года", "knowledge", "2", "иметь", "быть", "база", "написание",
-               "писать", "дать", "навык", "уровень", "образование", "years", "<highlighttext>development</highlighttext>", "с...",
-               "и...", "3-х","промышленный","3","базовый","паттерн","желание","данных","образование","разработки.","реляционный","как",
-               "структура","основный","система","код","strong","or","создание","современный","3.","good","проектирование","работы...'",
-               "необходимый","+", "лет.", "-", "х", "c", "5", "приложение", "разработчик", "rest", "development", "web"}
+    new_words = [word for word in words_lemmed if word not in other_words]
 
-requirement_list_lower = list(map(lambda x: x.lower(), requirement_list))
-words0 = []
-morph = pymorphy2.MorphAnalyzer()
-for i in requirement_list_lower:
-    words0.append(morph.parse(i)[0].normal_form)
-# print(requirement_list_lower)
-#print(len(requirement_list_lower))
-# for i in other_words:
-#     if i in words0:
-#         words0.remove(i)
-
-new_words = [word for word in words0 if word not in other_words]
-
-# print(words0)
+    # c = collections.Counter()
+    # for i in new_words:
+    #     c[i] +=1
 
 
+    c = collections.Counter(new_words)
 
-#3) получить из list пункта 3 dict, ключами которого являются слова,
-# а значениями их количество появлений в тексте;
+    results = c.most_common(10)
+    results_list = []
 
-
-
-text_dict = {a: new_words.count(a) for a in new_words}
-#print(text_dict)
-# for keys, values in text_dict.items():
-#     print(keys, values)
-popular_words = []
-for a in text_dict.values():
-    popular_words.append(a)
-popular_words.sort()
-
-popular_10words = popular_words[-10:]
-
-for keys, values in text_dict.items():
-    if values in popular_10words:
-        print(keys, values)
-
-print()
-
-# words = dict()
-# c = collections.Counter(new_words)
-# for key, value in c.items():
-#     words[key] = value
-#
-# print(c.most_common(100))
+    for tuple_ in results:
+        for i in range(len(tuple_)+1):
+            if i == 0:
+                results_list.append(tuple_[i])
 
 
+    return results_all, results_list
 
 
+a, b = hh_requirements()
+
+print(a, b)
